5e38062f230c4e9568e349492100cd5ceec99735
sugu <sugukumar778@gmail.com>
2026-02-10 09:12:40 +0530

    Fix CORS for Vercel frontend

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://sindhuja-colloction.vercel.app",
+      "https://sindhuja-frontend.vercel.app",
       "http://localhost:5173"
     ],
     credentials: true
